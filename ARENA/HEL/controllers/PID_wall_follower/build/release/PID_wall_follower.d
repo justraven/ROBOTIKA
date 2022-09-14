3d237d34f96b9c1e4478d899d3835a5573da10a3
alimsatria <alimsatria2017@mail.ugm.ac.id>
2022-09-14 11:38:27 +0700

    delete ARENA

--- ARENA/HEL/controllers/PID_wall_follower/build/release/PID_wall_follower.d
+++
@@ -1,6 +0,0 @@
-build/release/PID_wall_follower.o: PID_wall_follower.c \
- /snap/webots/20/usr/share/webots/include/controller/c/webots/robot.h \
- /snap/webots/20/usr/share/webots/include/controller/c/webots/types.h \
- /snap/webots/20/usr/share/webots/include/controller/c/webots/nodes.h \
- /snap/webots/20/usr/share/webots/include/controller/c/webots/distance_sensor.h \
- /snap/webots/20/usr/share/webots/include/controller/c/webots/motor.h
